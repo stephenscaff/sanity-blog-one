64d5bcd8eb155ccf0393e9bcac12c8fd96d15d97
Stephen Scaff <smscaff@gmail.com>
2020-12-23 06:32:29 -0800

    update to schema

---
+++ studio/schemas/product.js
@@ -0,0 +1,65 @@
+export default {
+  name: "post",
+  title: "Post",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+    },
+    {
+      name: "author",
+      title: "Author",
+      type: "reference",
+      to: { type: "author" },
+    },
+    {
+      name: "coverImage",
+      title: "Cover image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "categories",
+      title: "Categories",
+      type: "array",
+      of: [{ type: "reference", to: { type: "category" } }],
+    },
+    {
+      name: "publishedAt",
+      title: "Published at",
+      type: "datetime",
+    },
+    {
+      name: "content",
+      title: "Content",
+      type: "blockContent",
+    },
+  ],
+
+  preview: {
+    select: {
+      title: "title",
+      author: "author.name",
+      media: "CoverImage",
+    },
+    prepare(selection) {
+      const { author } = selection;
+      return Object.assign({}, selection, {
+        subtitle: author && `by ${author}`,
+      });
+    },
+  },
+};
